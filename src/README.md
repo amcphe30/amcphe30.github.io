3ff5355abc897c33b6bfeaebe95de738735f92dd
Abigail McPhee <abigailmcphee30@gmail.com>
2025-05-31 10:10:55 -0700

    Revert "React app"
    This reverts commit 4f34273670ec602032641808faec361f1bea0b11.

--- src/README.md
+++
@@ -1,3 +0,0 @@
-# Personal Portfolio Website
-
-I wanted something a bit more interesting than a Github repo to link to on job applications. Check it out: https://amcphe30.github.io./index.html
--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/amcphe30.github.io/',
-  plugins: [react()],
-});
